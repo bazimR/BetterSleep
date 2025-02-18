11e801a7e01c9d531ca94b389525b72e76d411f3
bazimR <rishaalmuhammed@gmail.com>
2025-02-18 05:51:27 +0530

    Initial Commit

---
+++ BetterSleep/BetterSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterSleepApp.swift
+//  BetterSleep
+//
+//  Created by Rishal Bazim on 18/02/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterSleepTests/BetterSleepTests.swift
@@ -0,0 +1,17 @@
+//
+//  BetterSleepTests.swift
+//  BetterSleepTests
+//
+//  Created by Rishal Bazim on 18/02/25.
+//
+
+import Testing
+@testable import BetterSleep
+
+struct BetterSleepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
